cd1afd0f680468695f869c277049e67f380677ef
Bepp1s <denivanov.denivanov@outlook.com>
2025-09-16 09:33:53 +0700

    Add files via upload

--- Matlab/PlotExp.m
+++ Matlab/PlotExp.m
@@ -1,12 +1,17 @@
-%Script puilds a plot of exponentional function which is set by constants
-%and value of t. Array of Exp. function is Generated in MassGen function
+%Script builds a plot of exponentional function which is set by constants
+% and value of t. Array of  Exp. function points is Generated in MassGen function
 
 %const. input
 Ts1=16;
 Ts2=5;
 %x range
-t=[-10:100];
+t=[-15:100];
 %function (y(x))
 Msv = MassGen(t,Ts1,Ts2);
+
+Trap = TrapFilter(t,Msv);
 %plot
+subplot(2,1,1);
 plot(t,Msv);
+subplot(2,1,2);
+plot(t,Trap);
