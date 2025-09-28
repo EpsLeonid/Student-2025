59ebd4fe173020b9e2a26c8cba4a995f4af92401
Bepp1s <denivanov.denivanov@outlook.com>
2025-09-28 19:53:44 +0700

    Changed on non-blocking assignment

--- Quartus II/Ivanov/Task2_1.sv
+++ Quartus II/Ivanov/Task2_1.sv
@@ -26,12 +26,12 @@ module Task2_1(
 	input [`DATA_WIDTH-1:0] a,
 	input [`DATA_WIDTH-1:0] b,
 	input [`DATA_WIDTH-1:0] c,
-	output [`DATA_WIDTH_OUT-1:0] data_out
+	output reg [`DATA_WIDTH_OUT-1:0] data_out
 );
 reg  [`DATA_WIDTH-1:0] q;
 	
 always_ff @(posedge clk) begin
-	q = c;
-	data_out = a*b+q;
+	q <= c;
+	data_out <= a*b+q;
 end
 endmodule
