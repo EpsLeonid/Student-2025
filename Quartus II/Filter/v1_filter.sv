127d9201f17afc24e600915b9da52ea46ca2ea93
Bepp1s <denivanov.denivanov@outlook.com>
2025-10-20 21:48:36 +0700

    Рабочее описание ПЛИС, при симуляции путаются старшие биты (симуляция зажёвывает старший бит)

--- Quartus II/Filter/v1_filter.sv
+++ Quartus II/Filter/v1_filter.sv
@@ -13,9 +13,9 @@ reg [3:0] i;
 
 reg [SIZE_ADC_DATA:0] d [1:0];
 
-reg [SIZE_ADC_DATA+1:0] p [1:0];
-reg [SIZE_ADC_DATA+6:0] r[1:0];
-reg [SIZE_ADC_DATA+7:0] Trap [2:0];
+reg [SIZE_ADC_DATA+2:0] p [1:0];
+reg [SIZE_ADC_DATA+4:0] r [1:0];
+reg [SIZE_ADC_DATA+5:0] Trap [1:0];
 
 reg [SIZE_ADC_DATA+1:0] sh1 [1:0];
 reg [SIZE_ADC_DATA+1:0] sh2 [1:0];
@@ -38,37 +38,15 @@ always_ff @(posedge clk)
 			end
 			data[0] <= input_data;
 	//
-			if ((cnt <= 5) & (input_data!=0))
-			begin//
-				d[0] <= data[0];
-				cnt <= cnt+1;
-			end
-			else if ((cnt <= 8) & (cnt > 5))
-			begin
-				d[0] <= data[0] - data[5];
-				cnt <= cnt+1;
-			end
-			else if ((cnt > 8) & (cnt <= 12))
-			begin
-				d[0] <= data[0] - data[5] - data[8];
-				cnt <= cnt+1;
-			end
-			else 
-			begin
-				d[0] <= data[0] - data[5] - data[8] + data[13];
-			end
-	
 			
-				p[0] <= p[1] + d[0];
-				r[0] <= p[0] + 16 * sh1[0];
-				Trap[0] <= Trap[1] + r[0];
+			d[0] <= data[0] - data[5] - data[8] + data[13];
+						
+			p[0] <= p[0] + d[0];
+			r[0] <= p[0] + 16 * sh1[0];
+			Trap[0] <= Trap[0] + r[0];
+	
+			sh1[0] <= d[0];
 
-				p[1] <= p[0];
-				r[1] <= r[0];
-				d[1] <= d[0];
-				
-				sh1[0]<= d[0];
-				
 			output_data <= Trap[0];
 		end
 	end
