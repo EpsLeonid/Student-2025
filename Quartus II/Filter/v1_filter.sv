eee1a545d35d998040665fd1daa49d58c01133d5
Bepp1s <denivanov.denivanov@outlook.com>
2025-11-18 13:35:22 +0700

    Add files via upload

--- Quartus II/Filter/v1_filter.sv
+++ Quartus II/Filter/v1_filter.sv
@@ -3,7 +3,7 @@ module v1_filter(
 	input wire clk,
 	input wire reset,
 	input wire [SIZE_IN_DATA-1:0] input_data,
-	output wire [SIZE_IN_DATA+5:0] output_data
+	output wire [SIZE_IN_DATA-1:0] output_data
 );
 reg [SIZE_IN_DATA-1:0] data [DEPTH-1:0];
 reg [SIZE_CNT-1:0] cnt;
@@ -12,11 +12,9 @@ reg [SIZE_CNT-1:0] i;
 
 reg [SIZE_IN_DATA:0] d;
 
-reg [SIZE_IN_DATA:0] sh;
-
 reg [SIZE_IN_DATA+2:0] p;
 reg [SIZE_IN_DATA+4:0] r;
-reg [SIZE_IN_DATA+5:0] Trap;
+reg [SIZE_IN_DATA+7:0] Trap;
 
 always_ff @(posedge clk) 
 	begin
@@ -44,14 +42,13 @@ always_ff @(posedge clk)
 
 			d <= data[0] - data[k] - data[l] + data[k+l];
 			
-			sh <= d;
 
 			p <= p + d;
 			r <= p + M * sh;
 			Trap <= Trap + r;
 
 
-			output_data <= Trap;
+			output_data <= Trap[SIZE_IN_DATA+7:8];
 		end
 	end
 endmodule
